e0c075a49296f3fa19c299b0f0361ed1f0336fbf
Ágota Ékes <ekesagi@gmail.com>
2020-12-03 21:54:47 +0100

    Osztályok létrehozása

---
+++ IRF_beadando/IRF_beadando/Buszjarat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_beadando
+{
+    public class Buszjarat
+    {
+        public DateTime IndulasiIdo;
+        public DateTime ErkezesiIdo;
+        public int Idotartam;
+        public Varos IndulasiHely;
+        public Varos ErkezesiHely;
+        public int UtHossz;
+    }
+}
---
+++ IRF_beadando/IRF_beadando/Varos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_beadando
+{
+    public class Varos
+    {
+        public string Nev;
+    }
+}
