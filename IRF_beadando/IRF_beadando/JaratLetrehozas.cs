d2bae1a1ac56669010852acb62328eeffe1a3fee
Ágota Ékes <ekesagi@gmail.com>
2020-12-14 21:29:24 +0100

    Ürítés funkció

--- IRF_beadando/IRF_beadando/JaratLetrehozas.cs
+++ IRF_beadando/IRF_beadando/JaratLetrehozas.cs
@@ -26,50 +26,68 @@ namespace IRF_beadando
 
         private void btnMentes_Click(object sender, EventArgs e)
         {
-            if (cbJarmu.SelectedItem.ToString() == "Busz")
+            try
             {
-                Buszjarat bj = new Buszjarat();
-                bj.ErkezesiIdo = dtpErkezesiIdo.Value;
-                bj.IndulasiIdo = dtpInduasiIdo.Value;
-                bj.ErkezesiHely = cbErkezoVaros.SelectedItem.ToString();
-                bj.IndulasiHely = cbKiinduloVaros.SelectedItem.ToString();
-                bj.UtHossz = Convert.ToInt32(tbUthossz.Text);
-                bj.Idotartam = (int)Math.Floor((bj.ErkezesiIdo - bj.IndulasiIdo).TotalMinutes);
-                bj.AutoPalyanMegyE = cbAutoPalyanMegyE.Checked;
-                Form1.Jaratok.Add(bj);
-                MessageBox.Show("Buszjárat hozzáadva. Új járat megadásához, kérem töltse ki újból a formot!");
+                if (cbJarmu.SelectedItem.ToString() == "Busz")
+                {
+                    Buszjarat bj = new Buszjarat();
+                    bj.ErkezesiIdo = dtpErkezesiIdo.Value;
+                    bj.IndulasiIdo = dtpInduasiIdo.Value;
+                    bj.ErkezesiHely = cbErkezoVaros.SelectedItem.ToString();
+                    bj.IndulasiHely = cbKiinduloVaros.SelectedItem.ToString();
+                    bj.UtHossz = Convert.ToInt32(tbUthossz.Text);
+                    bj.Idotartam = (int)Math.Floor((bj.ErkezesiIdo - bj.IndulasiIdo).TotalMinutes);
+                    bj.AutoPalyanMegyE = cbAutoPalyanMegyE.Checked;
+                    Form1.Jaratok.Add(bj);
+                    MessageBox.Show("Buszjárat hozzáadva. Új járat megadásához, kérem töltse ki újból a formot!");
+                    Urites();
+                }
+                else if (cbJarmu.SelectedItem.ToString() == "Repülő")
+                {
+                    Repulojarat rj = new Repulojarat();
+                    rj.ErkezesiIdo = dtpErkezesiIdo.Value;
+                    rj.IndulasiIdo = dtpInduasiIdo.Value;
+                    rj.ErkezesiHely = cbErkezoVaros.SelectedItem.ToString();
+                    rj.IndulasiHely = cbKiinduloVaros.SelectedItem.ToString();
+                    rj.UtHossz = Convert.ToInt32(tbUthossz.Text);
+                    rj.Idotartam = (int)Math.Floor((rj.ErkezesiIdo - rj.IndulasiIdo).TotalMinutes);
+                    rj.KapunkEEbedet = cbKapunkEEbedet.Checked;
+                    Form1.Jaratok.Add(rj);
+                    MessageBox.Show("Repülőjárat hozzáadva. Új járat megadásához, kérem töltse ki újból a formot!");
+                    Urites();
+                }
+                else
+                {
+                    Vonatjarat vj = new Vonatjarat();
+                    vj.ErkezesiIdo = dtpErkezesiIdo.Value;
+                    vj.IndulasiIdo = dtpInduasiIdo.Value;
+                    vj.ErkezesiHely = cbErkezoVaros.SelectedItem.ToString();
+                    vj.IndulasiHely = cbKiinduloVaros.SelectedItem.ToString();
+                    vj.UtHossz = Convert.ToInt32(tbUthossz.Text);
+                    vj.Idotartam = (int)Math.Floor((vj.ErkezesiIdo - vj.IndulasiIdo).TotalMinutes);
+                    vj.Tipus = cbVonatTipusa.SelectedItem.ToString();
+                    Form1.Jaratok.Add(vj);
+                    MessageBox.Show("Vonatjárat hozzáadva. Új járat megadásához, kérem töltse ki újból a formot!");
+                    Urites();
+                }
             }
-            else if (cbJarmu.SelectedItem.ToString() == "Repülő")
+            catch (Exception)
             {
-                Repulojarat rj = new Repulojarat();
-                rj.ErkezesiIdo = dtpErkezesiIdo.Value;
-                rj.IndulasiIdo = dtpInduasiIdo.Value;
-                rj.ErkezesiHely = cbErkezoVaros.SelectedItem.ToString();
-                rj.IndulasiHely = cbKiinduloVaros.SelectedItem.ToString();
-                rj.UtHossz = Convert.ToInt32(tbUthossz.Text);
-                rj.Idotartam = (int)Math.Floor((rj.ErkezesiIdo - rj.IndulasiIdo).TotalMinutes);
-                rj.KapunkEEbedet = cbKapunkEEbedet.Checked;
-                Form1.Jaratok.Add(rj);
-                MessageBox.Show("Repülőjárat hozzáadva. Új járat megadásához, kérem töltse ki újból a formot!");
+                MessageBox.Show("Kérem töltsön ki minden mezőt!");
             }
-            else
-            {
-                Vonatjarat vj = new Vonatjarat();
-                vj.ErkezesiIdo = dtpErkezesiIdo.Value;
-                vj.IndulasiIdo = dtpInduasiIdo.Value;
-                vj.ErkezesiHely = cbErkezoVaros.SelectedItem.ToString();
-                vj.IndulasiHely = cbKiinduloVaros.SelectedItem.ToString();
-                vj.UtHossz = Convert.ToInt32(tbUthossz.Text);
-                vj.Idotartam = (int)Math.Floor((vj.ErkezesiIdo - vj.IndulasiIdo).TotalMinutes);
-                vj.Tipus = cbVonatTipusa.SelectedItem.ToString();
-                Form1.Jaratok.Add(vj);
-                MessageBox.Show("Vonatjárat hozzáadva. Új járat megadásához, kérem töltse ki újból a formot!");
-            }
+            
         }
 
         private void cbJarmu_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (cbJarmu.SelectedItem.ToString() == "Busz")
+            if (cbJarmu.SelectedItem ==null)
+            {
+                lblVonatTipusa.Visible = false;
+                cbVonatTipusa.Visible = false;
+                cbKapunkEEbedet.Visible = false;
+                cbAutoPalyanMegyE.Visible = false;
+            }
+            else if (cbJarmu.SelectedItem.ToString() == "Busz")
             {
                 lblVonatTipusa.Visible = false;
                 cbVonatTipusa.Visible = false;
@@ -90,6 +108,20 @@ namespace IRF_beadando
                 cbKapunkEEbedet.Visible = false;
                 cbAutoPalyanMegyE.Visible = false;
             }
+        }
+
+        private void Urites()
+        {
+            cbJarmu.SelectedItem = null;
+            cbKiinduloVaros.SelectedItem = null;
+            cbErkezoVaros.SelectedItem = null;
+            cbVonatTipusa.SelectedItem=null ;
+            cbKapunkEEbedet.Checked = false;
+            cbAutoPalyanMegyE.Checked = false;
+            tbUthossz.Clear();
+            dtpInduasiIdo.Value = DateTime.Now.Date;
+            dtpErkezesiIdo.Value = DateTime.Now.Date;
+
         }
     }
 }
